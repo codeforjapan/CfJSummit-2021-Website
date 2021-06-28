3c3b9dadd400318db57d2d5d8313bd2fd605d712
Taiyu Yoshizawa <taiyu31@gmail.com>
2021-06-28 09:46:19 +0900

    fix: logos

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 import { VFC } from 'react'
 import styles from '~/styles/components/LogoSection.module.scss'
 import Image from 'next/image'
-import LogoPNG from '~/assets/cfj-summit-2021_logo_rectangle--white.png'
+import LogoPNG from '~/assets/cfj-summit-2021_logo_rectangle-white.png'
 
 const Logo: VFC = () => {
   return (
