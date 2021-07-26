8189b0c6376c8ae9fdd0e7aef73bbde7de5c6be6
Taiyu Yoshizawa <taiyu31@gmail.com>
2021-07-26 12:14:19 +0900

    add: ScrollDown Effect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,6 @@ const Header: VFC = () => {
         <video src="./hero.mp4" playsInline muted autoPlay />
       </div>
       <section className={styles.headerTitle}>
-        <h1>Code for Japan Summit</h1>
         <h2>2021.9.18(sat.)-19(sun.)</h2>
         <p>@オンライン開催</p>
       </section>
